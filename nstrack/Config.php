cd78f23c57d9708deff5f27da28197b0702818c9
Andrew Watts <andrew@karmabunny.com.au>
2015-11-11 17:30:52 +1030

    Finished converting tabs to 4 spaces.

--- nstrack/Config.php
+++ nstrack/Config.php
@@ -10,96 +10,96 @@
  * Config options when running nstrack
  */
 class Config {
-	private static $src_dir;
-	private static $ignore = [];
-	private static $sort_function;
-	private static $group_function;
+    private static $src_dir;
+    private static $ignore = [];
+    private static $sort_function;
+    private static $group_function;
 
-	/**
-	 * @param array $a Classes associated with a use statement
-	 * @param array $b Classes associated with a use statement
-	 */
-	static function default_sort($a, $b) {
-		if (is_array($a)) $a = array_shortest($a);
-		if (is_array($b)) $b = array_shortest($b);
-		$a_has_ns = (strpos($a, '\\') !== false);
-		$b_has_ns = (strpos($b, '\\') !== false);
-		if (!$a_has_ns and $b_has_ns) return -1;
-		if ($a_has_ns and !$b_has_ns) return 1;
+    /**
+     * @param array $a Classes associated with a use statement
+     * @param array $b Classes associated with a use statement
+     */
+    static function default_sort($a, $b) {
+        if (is_array($a)) $a = array_shortest($a);
+        if (is_array($b)) $b = array_shortest($b);
+        $a_has_ns = (strpos($a, '\\') !== false);
+        $b_has_ns = (strpos($b, '\\') !== false);
+        if (!$a_has_ns and $b_has_ns) return -1;
+        if ($a_has_ns and !$b_has_ns) return 1;
 
-		if ($a < $b) return -1;
-		if ($a > $b) return 1;
-		return 0;
-	}
+        if ($a < $b) return -1;
+        if ($a > $b) return 1;
+        return 0;
+    }
 
-	/**
-	 * @param array $classes Classes associated with a use statement
-	 * @param bool $has_ns True if any of the classes include a namespace
-	 *        separator \
-	 * @return string Group name
-	 */
-	static function default_group(array $classes, $has_ns) {
-		return '';
-	}
+    /**
+     * @param array $classes Classes associated with a use statement
+     * @param bool $has_ns True if any of the classes include a namespace
+     *        separator \
+     * @return string Group name
+     */
+    static function default_group(array $classes, $has_ns) {
+        return '';
+    }
 
-	static function dir() { return self::$src_dir; }
-	static function ignore() { return self::$ignore; }
+    static function dir() { return self::$src_dir; }
+    static function ignore() { return self::$ignore; }
 
-	/**
-	 * @return callable Function to sort use statements
-	 */
-	static function sort() {
-		if (!empty(self::$sort_function)) return self::$sort_function;
-		return self::default_sort;
-	}
+    /**
+     * @return callable Function to sort use statements
+     */
+    static function sort() {
+        if (!empty(self::$sort_function)) return self::$sort_function;
+        return self::default_sort;
+    }
 
-	/**
-	 * @return callable Function to split use blocks into groups
-	 */
-	static function group() {
-		if (!empty(self::$group_function)) return self::$group_function;
-		return self::default_group;
-	}
+    /**
+     * @return callable Function to split use blocks into groups
+     */
+    static function group() {
+        if (!empty(self::$group_function)) return self::$group_function;
+        return self::default_group;
+    }
 
-	/**
-	 * Sets the directory which contains the source code
-	 * @param string $dir
-	 * @return void
-	 */
-	static function setSourceDir($dir) {
-		self::$src_dir = realpath($dir) . '/';
-	}
+    /**
+     * Sets the directory which contains the source code
+     * @param string $dir
+     * @return void
+     */
+    static function setSourceDir($dir) {
+        self::$src_dir = realpath($dir) . '/';
+    }
 
-	/**
-	 * Sets the list of classes to ignore
-	 * @param array $ignore Class names (excluding namespaces)
-	 * @return void
-	 */
-	static function setIgnore(array $ignore) {
-		self::$ignore = $ignore;
-	}
+    /**
+     * Sets the list of classes to ignore
+     * @param array $ignore Class names (excluding namespaces)
+     * @return void
+     */
+    static function setIgnore(array $ignore) {
+        self::$ignore = $ignore;
+    }
 
-	/**
-	 * Sets the list of classes to ignore
-	 * @param array $ignore Class names (excluding namespaces)
-	 * @return void
-	 */
-	static function setSort(callable $func) {
-		self::$sort_function = $func;
-	}
+    /**
+     * Sets the list of classes to ignore
+     * @param array $ignore Class names (excluding namespaces)
+     * @return void
+     */
+    static function setSort(callable $func) {
+        self::$sort_function = $func;
+    }
 
-	static function setGroup(callable $func) {
-		self::$group_function = $func;
-	}
+    static function setGroup(callable $func) {
+        self::$group_function = $func;
+    }
 
-	/**
-	 * Loads a config file.
-	 * The file should call Config::set* methods
-	 * @param string $file Path to the config file
-	 * @return void
-	 */
-	static function load($file) {
-		if (file_exists($file)) require $file;
-		else die ('Error: no configuration file found! You need a ".nstrack.php" file in the current directory.' . PHP_EOL);
-	}
+    /**
+     * Loads a config file.
+     * The file should call Config::set* methods
+     * @param string $file Path to the config file
+     * @return void
+     */
+    static function load($file) {
+        if (file_exists($file)) require $file;
+        else die ('Error: no configuration file found! You need a ".nstrack.php" file in the current directory.' . PHP_EOL);
+    }
 }
--- nstrack/UseStatement.php
+++ nstrack/UseStatement.php
@@ -7,21 +7,21 @@
 
 
 class UseStatement {
-	public $entity;
-	public $alias;
-	
-	/**
-	 * @param string $entity The entity, e.g. AwesomeThings\SuperThing
-	 * @param string $alias The alias, i.e. 'as ___'
-	 */
-	function __construct($entity, $alias) {
-		$this->entity = $entity;
-		$this->alias = $alias;
-	}
-	
-	function __toString() {
-		$str = $this->entity;
-		if ($this->alias) $str .= ' as ' . $this->alias;
-		return $str;
-	}
+    public $entity;
+    public $alias;
+    
+    /**
+     * @param string $entity The entity, e.g. AwesomeThings\SuperThing
+     * @param string $alias The alias, i.e. 'as ___'
+     */
+    function __construct($entity, $alias) {
+        $this->entity = $entity;
+        $this->alias = $alias;
+    }
+    
+    function __toString() {
+        $str = $this->entity;
+        if ($this->alias) $str .= ' as ' . $this->alias;
+        return $str;
+    }
 }
