842925268083a065951826506a6112d8e909684a
Andrew Watts <andrew@karmabunny.com.au>
2015-11-11 17:24:36 +1030

    Added the targeted (-t, --targeted <rel path>), missing only (-m, --missing), needs only (-n, --needs), colours (-c, --colours) options. Fixed --write writing into non-PHP areas when the file has single-line <?php ?> blocks at the start. Also converted all the tabs to 4 spaces.

--- nstrack.php
+++ nstrack.php
@@ -27,7 +27,23 @@ Config::load($dir . '.nstrack.php');
 $write = (in_array('-w', $argv) or in_array('--write', $argv));
 $verbose = (in_array('-v', $argv) or in_array('--verbose', $argv));
 $debug = (in_array('-d', $argv) or in_array('--debug', $argv));
+$missing_only = (in_array('-m', $argv) or in_array('--missing', $argv));
+$needs_only = (in_array('-n', $argv) or in_array('--needs', $argv));
+$use_colours = (in_array('-c', $argv) or in_array('--colours', $argv));
+$targeted = array_search('--targeted', $argv);
+$target_dir = null;
 if ($debug) $verbose = true;
+
+if ($missing_only and $needs_only) {
+    die('You can\'t have it both ways. Pick one or none: -m or -n' . PHP_EOL);
+}
+
+if ($targeted) {
+    if ($argc <= $targeted + 1) die('--targeted requires a directory argumemt' . PHP_EOL);
+    
+    $target_dir = escapeshellarg(Config::dir() . $argv[$targeted + 1]);
+    $targeted = true;
+}
 
 $cmd = "find " . Config::dir() . " -name '*.php'";
 $files = [];
@@ -36,41 +52,41 @@ exec($cmd, $file_names);
 
 $file_count = 0;
 foreach ($file_names as $file) {
-	
-	$data = ParsedFile::parse($file);
-	$files[$file] = $data;
-	
-	if ($data->isEmpty()) continue;
-	
-	if (count($data->namespaces) > 1) {
-		echo '*** ', $file, "\n";
-		echo 'namespaces: ', implode(', ', $data->namespaces), "\n";
-		die("ERROR: multiple namespace declarations. Don't do this!");
-	}
-	
-	if ($verbose) {
-		echo '*** ', $file, "\n";
-		echo 'namespaces: ', implode(', ', $data->namespaces), "\n";
-		echo 'uses: ', implode(', ', $data->uses), "\n";
-		echo 'classes: ', implode(', ', $data->classes), "\n";
-		echo 'refs: ', implode(', ', $data->refs), "\n";
-		echo "\n";
-	}
+    
+    $data = ParsedFile::parse($file);
+    $files[$file] = $data;
+    
+    if ($data->isEmpty()) continue;
+    
+    if (count($data->namespaces) > 1) {
+        echo '*** ', $file, "\n";
+        echo 'namespaces: ', implode(', ', $data->namespaces), "\n";
+        die("ERROR: multiple namespace declarations. Don't do this!");
+    }
+    
+    if ($verbose) {
+        echo '*** ', $file, "\n";
+        echo 'namespaces: ', implode(', ', $data->namespaces), "\n";
+        echo 'uses: ', implode(', ', $data->uses), "\n";
+        echo 'classes: ', implode(', ', $data->classes), "\n";
+        echo 'refs: ', implode(', ', $data->refs), "\n";
+        echo "\n";
+    }
 }
 
 /** Mapping of base class name => array of namespaced classnames, e.g.
     'AwesomeClass' => [Awesome\\AwesomeClass, MoreDifferent\\AwesomeClass] */
 $full_classes = [];
 foreach ($internal_classes as $class) {
-	if (!isset($full_classes[$class])) $full_classes[$class] = [];
-	$full_classes[$class][] = $class;
+    if (!isset($full_classes[$class])) $full_classes[$class] = [];
+    $full_classes[$class][] = $class;
 }
 foreach ($files as $file) {
-	$namespace = '';
-	if (count($file->namespaces) > 0) $namespace = $file->namespaces[0];
+    $namespace = '';
+    if (count($file->namespaces) > 0) $namespace = $file->namespaces[0];
     foreach ($file->classes as $class) {
-    	$full_class = $class;
-    	if ($namespace) $full_class = "{$namespace}\\{$class}";
+        $full_class = $class;
+        if ($namespace) $full_class = "{$namespace}\\{$class}";
         if (!isset($full_classes[$class])) $full_classes[$class] = [];
         $full_classes[$class][] = $full_class;
     }
@@ -87,10 +103,18 @@ if ($debug) {
 
 $unknown_classes = [];
 
+if ($targeted) {
+    $cmd = "find {$target_dir} -name '*.php'";
+    $file_names = [];
+    exec($cmd, $file_names);
+    echo 'Restricting changes to: ', $target_dir, PHP_EOL;
+}
+
 // Compare use block with actual classes used
 echo "\n****************************************\n";
 $count = 0;
-foreach ($files as $file) {
+foreach ($file_names as $filename) {
+    $file = $files[$filename];
     $used_classes = $file->refs;
     
     $need = [];
@@ -113,43 +137,43 @@ foreach ($files as $file) {
         // Handle class with complete namespace reference,
         // e.g. \AwesomeProject\AwesomeThings\Awesome
         if ($class[0] == '\\') {
-        	$debug_text .= "    checking full class list... ";
-        	$full_ref = substr($class, 1);
-        	$stripped = rm_class_ns($full_ref);
-        	if (@in_array($full_ref, $full_classes[$stripped])) {
-        		$debug_text .= "OK (full reference)\n\n";
-        		continue;
-        	}
-        	$debug_text .= "no good\n";
+            $debug_text .= "    checking full class list... ";
+            $full_ref = substr($class, 1);
+            $stripped = rm_class_ns($full_ref);
+            if (@in_array($full_ref, $full_classes[$stripped])) {
+                $debug_text .= "OK (full reference)\n\n";
+                continue;
+            }
+            $debug_text .= "no good\n";
         }
         
         // Determine class using use statements
         $used = false;
         foreach ($file->uses as $use) {
-        	$debug_text .= "    against use: {$use}... ";
-        	if ($use->alias == $class) {
-        		$used = true;
-        		$debug_text .= "OK (matches use alias)\n\n";
-        		break;
-        	}
-        	$stripped = rm_class_ns($use->entity);
-        	$debug_text .= " stripped to {$stripped}... ";
-        	if ($stripped == $class) {
-        		$used = true;
-        		$debug_text .= "OK (matches use class)\n\n";
-        		break;
-        	} else {
-        		$pos = strrpos($class, '\\');
-        		if ($pos !== false) {
-        			$class_ns = substr($class, 0, $pos);
-        			if ($stripped == $class_ns) {
-        				$used = true;
-						$debug_text .= "OK (matches use NS)\n\n";
-						break;
-        			}
-        		}
-        	}
-        	$debug_text .= "no good\n";
+            $debug_text .= "    against use: {$use}... ";
+            if ($use->alias == $class) {
+                $used = true;
+                $debug_text .= "OK (matches use alias)\n\n";
+                break;
+            }
+            $stripped = rm_class_ns($use->entity);
+            $debug_text .= " stripped to {$stripped}... ";
+            if ($stripped == $class) {
+                $used = true;
+                $debug_text .= "OK (matches use class)\n\n";
+                break;
+            } else {
+                $pos = strrpos($class, '\\');
+                if ($pos !== false) {
+                    $class_ns = substr($class, 0, $pos);
+                    if ($stripped == $class_ns) {
+                        $used = true;
+                        $debug_text .= "OK (matches use NS)\n\n";
+                        break;
+                    }
+                }
+            }
+            $debug_text .= "no good\n";
         }
         if ($used) continue;
         
@@ -174,31 +198,40 @@ foreach ($files as $file) {
     }
     
     $display = false;
-    if (count($need) > 0) $display = true;
-    if (count($missing) > 0) $display = true;
+    if (count($need) > 0 and !$missing_only) $display = true;
+    if (count($missing) > 0 and !$needs_only) $display = true;
     if ($debug) $display = true;
     
     if (!$display) continue;
     
     ++$count;
-    echo "[{$count}]\n";
-    echo "{$file->file}\n";
+    if ($use_colours) echo "\033[1;34m[{$count}] {$file->file}\033[0m", PHP_EOL;
+    else echo "[{$count}] {$file->file}", PHP_EOL;
+    
     if ($namespace and $verbose) echo "(namespace $namespace)\n";
     if ($debug) {
-    	echo "Existing use statements:\n";
-    	foreach ($file->uses as $use_st) {
-    		echo "    use ", $use_st, "\n";
-    	}
-    	echo $debug_text;
+        echo "Existing use statements:\n";
+        foreach ($file->uses as $use_st) {
+            echo "    use ", $use_st, "\n";
+        }
+        echo $debug_text;
     }
-    if (count($missing) > 0) {
+
+    if (count($missing) > 0 and !$needs_only) {
         echo "MISSING:\n";
         $lines = explode("\n", $file->content);
         foreach ($missing as $class_ref) {
-            echo "{$class_ref->class}, line {$class_ref->line}: ";
+            if ($use_colours) echo "\033[1;31m{$class_ref->class}\033[0m, line {$class_ref->line}: ";
+            else echo "{$class_ref->class}, line {$class_ref->line}: ";
             echo trim($lines[$class_ref->line - 1]), "\n";
         }
     }
+    
+    if ($missing_only) {
+        echo PHP_EOL;
+        continue;
+    }
+    
     if ($verbose) {
         echo "Use: [" . implode(', ', $file->uses) . "]\n";
         echo "Actual use: [", implode(', ', $used_classes), "]\n";
@@ -207,7 +240,7 @@ foreach ($files as $file) {
     
     // Incorporate extant use statements to create a complete list
     foreach ($file->uses as $use_st) {
-    	$need[] = [(string) $use_st];
+        $need[] = [(string) $use_st];
     }
     
     usort($need, Config::sort());
@@ -216,27 +249,28 @@ foreach ($files as $file) {
     $section = '';
     $last_section = '';
     foreach ($need as $classes) {
-    	$has_ns = false;
-    	foreach ($classes as $class) {
-    		if (strpos($class, '\\') !== false) {
-    			$has_ns = true;
-    			break;
-    		}
-    	}
-    	
-    	$groupify = Config::group();
-    	$section = $groupify($classes, $has_ns);
-    	
-    	if ($section != $last_section) {
-    		if ($last_section != '') $use_block .= "\n";
-    		$last_section = $section;
-    	}
-    	
+        $has_ns = false;
+        foreach ($classes as $class) {
+            if (strpos($class, '\\') !== false) {
+                $has_ns = true;
+                break;
+            }
+        }
+        
+        $groupify = Config::group();
+        $section = $groupify($classes, $has_ns);
+        
+        if ($section != $last_section) {
+            if ($last_section != '') $use_block .= "\n";
+            $last_section = $section;
+        }
+        
         $use_block .= "use ";
         $class = array_shortest($classes);
-		if ($class[0] == '\\') $class = substr($class, 1);
-		$use_block .= $class . ";\n";
+        if ($class[0] == '\\') $class = substr($class, 1);
+        $use_block .= $class . ";\n";
     }
+    
     echo $use_block;
     if ($write and $use_block) write_use_block($use_block, $file->file);
     echo "\n****************************************\n";
--- nstrack/functions.php
+++ nstrack/functions.php
@@ -30,9 +30,9 @@ function token_str($tok) {
  * @return string
  */
 function rm_class_ns($class) {
-	$pos = strrpos($class, '\\');
-	if ($pos === false) return $class;
-	return substr($class, $pos + 1);
+    $pos = strrpos($class, '\\');
+    if ($pos === false) return $class;
+    return substr($class, $pos + 1);
 }
 
 
@@ -40,14 +40,14 @@ function rm_class_ns($class) {
  * Finds the shortest element of an array
  */
 function array_shortest($arr) {
-	$len = 10000;
-	$short = false;
-	foreach ($arr as $el) {
-		if (strlen($el) >= $len) continue;
-		$short = $el;
-		$len = strlen($el);
-	}
-	return $short;
+    $len = 10000;
+    $short = false;
+    foreach ($arr as $el) {
+        if (strlen($el) >= $len) continue;
+        $short = $el;
+        $len = strlen($el);
+    }
+    return $short;
 }
 
 
@@ -58,21 +58,27 @@ function array_shortest($arr) {
  * @return void writes to the specified file
  */
 function write_use_block($block, $file) {
-	$lines = file($file);
-	$in_php = false;
+    $lines = file($file);
+    $in_php = false;
     $in_comment = false;
-	$code_start = -1;
-	$ns = -1;
-	$use_start = -1;
-	$use_end = -1;
-	foreach ($lines as $num => $line) {
-		if (preg_match('/^\s*<\?php/', $line)) {
-			$in_php = true;
-			continue;
-		}
-		if (preg_match('/^\s*$/', $line)) continue;
+    $code_start = -1;
+    $ns = -1;
+    $use_start = -1;
+    $use_end = -1;
+    foreach ($lines as $num => $line) {
+        if (preg_match('/^\s*<\?php.*?>\s*$/', $line)) {
+            $code_start = $num ? $num - 1 : 0;
+            $in_php = false;
+            break;
+        }
+        
+        if (preg_match('/^\s*<\?php/', $line)) {
+            $in_php = true;
+            continue;
+        }
+        if (preg_match('/^\s*$/', $line)) continue;
 
-		if ($in_php) {
+        if ($in_php) {
             if ($in_comment) {
                 if (preg_match('!\*/!', $line)) {
                     $in_comment = false;
@@ -90,56 +96,56 @@ function write_use_block($block, $file) {
                 continue;
             }
 
-			if (preg_match('/^\s*namespace\s/', $line)) {
-				$ns = $num;
-				continue;
-			}
+            if (preg_match('/^\s*namespace\s/', $line)) {
+                $ns = $num;
+                continue;
+            }
 
-			if (preg_match('/^\s*use\s/', $line)) {
-				if ($use_start < 0) $use_start = $num;
-				continue;
-			}
+            if (preg_match('/^\s*use\s/', $line)) {
+                if ($use_start < 0) $use_start = $num;
+                continue;
+            }
 
-			$use_end = $num;
-		}
+            $use_end = $num;
+        }
 
-		$code_start = $num;
-		break;
-	}
+        $code_start = $num;
+        break;
+    }
 
-	if ($use_start > 0) {
-		$start = rtrim(implode(array_slice($lines, 0, $use_start), ''));
-		if ($ns > 0) {
-			$start .= "\n\n";
-		} else {
-			$start .= "\n";
-		}
-		$end = "\n\n" . ltrim(implode(array_slice($lines, $use_end), ''));
-	} else if ($ns > 0) {
-		$start = rtrim(implode(array_slice($lines, 0, $ns + 1), '')) . "\n\n";
-		$end = "\n\n" . ltrim(implode(array_slice($lines, $ns + 1), ''));
-	} else if ($code_start >= 0) {
-		$end = ltrim(implode(array_slice($lines, $code_start), ''));
-		if ($in_php) {
-			$start = rtrim(implode(array_slice($lines, 0, $code_start), ''));
-			if ($start != '<?php') $start .= "\n";
-			$start .= "\n";
-			if (substr($end, 0, 2) != '?>') $end = "\n\n" . $end;
-		} else {
-			$start = "<?php\n";
-			$end = "?>\n" . $end;
-		}
-	} else {
-		$err = "Unable to determine use block position\n";
-		$err .= "in_php: $in_php\n";
-		$err .= "code_start: $code_start\n";
-		$err .= "ns: $ns\n";
-		$err .= "use_start: $use_start\n";
-		$err .= "use_end: $use_end\n";
-		die($err);
-		throw new Exception($err);
-	}
-	$new_content = $start . $block . $end;
+    if ($use_start > 0) {
+        $start = rtrim(implode(array_slice($lines, 0, $use_start), ''));
+        if ($ns > 0) {
+            $start .= "\n\n";
+        } else {
+            $start .= "\n";
+        }
+        $end = "\n\n" . ltrim(implode(array_slice($lines, $use_end), ''));
+    } else if ($ns > 0) {
+        $start = rtrim(implode(array_slice($lines, 0, $ns + 1), '')) . "\n\n";
+        $end = "\n\n" . ltrim(implode(array_slice($lines, $ns + 1), ''));
+    } else if ($code_start >= 0) {
+        $end = ltrim(implode(array_slice($lines, $code_start), ''));
+        if ($in_php) {
+            $start = rtrim(implode(array_slice($lines, 0, $code_start), ''));
+            if ($start != '<?php') $start .= "\n";
+            $start .= "\n";
+            if (substr($end, 0, 2) != '?>') $end = "\n\n" . $end;
+        } else {
+            $start = "<?php\n";
+            $end = "?>\n" . $end;
+        }
+    } else {
+        $err = "Unable to determine use block position\n";
+        $err .= "in_php: $in_php\n";
+        $err .= "code_start: $code_start\n";
+        $err .= "ns: $ns\n";
+        $err .= "use_start: $use_start\n";
+        $err .= "use_end: $use_end\n";
+        die($err);
+        throw new Exception($err);
+    }
+    $new_content = $start . $block . $end;
 
-	file_put_contents($file, $new_content);
+    file_put_contents($file, $new_content);
 }
