0ca76e282a5fe3368f4a06e438b932ca7c201a35
Josh Heidenreich <josh@nationalcrimecheck.com.au>
2024-07-19 09:29:56 +0930

    Move the test *data* into it's own direcotry
    This clarifies which files are tests and which files are data used
    by tests

--- tests/nstrackTest.php
+++ tests/nstrackTest.php
@@ -10,13 +10,13 @@ class nstrackTest extends TestCase
 
     public function testTypeHints()
     {
-        $file = ParsedFile::parse(__DIR__ . '/classes_type_hints.php');
+        $file = ParsedFile::parse(__DIR__ . '/data/classes_type_hints.php');
         $this->assertEquals(3, count($file->refs));
     }
 
     public function testParseNestedNs()
     {
-        $file = ParsedFile::parse(__DIR__ . '/nested_ns.php');
+        $file = ParsedFile::parse(__DIR__ . '/data/nested_ns.php');
         $this->assertEquals(['Nested\\Namespace\\Structure'], $file->namespaces);
     }
 
