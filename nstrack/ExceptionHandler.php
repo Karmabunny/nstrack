db6662f1579c033c1bde8cdf86d333a3ca4fca01
Josh Heidenreich <josh@nationalcrimecheck.com.au>
2025-03-27 09:48:51 +1030

    System for "context" to be added for exception reporting

--- nstrack/ExceptionHandler.php
+++ nstrack/ExceptionHandler.php
@@ -2,6 +2,8 @@
 
 class ExceptionHandler
 {
+    private static $context = [];
+
 
     /**
      * Setup error and exception handling
@@ -37,8 +39,40 @@ class ExceptionHandler
         echo $ex->getMessage(), PHP_EOL;
         echo ' at ', $ex->getFile() . ':', $ex->getLine(), PHP_EOL;
         echo 'Stack Trace:', PHP_EOL;
-        echo $ex->getTraceAsString(), PHP_EOL, PHP_EOL;
+        echo ' ', str_replace("\n", "\n ", $ex->getTraceAsString()), PHP_EOL;
+        if (!empty(self::$context)) echo 'Context:', PHP_EOL;
+        foreach (self::$context as $key => $val) {
+            echo ' ', $key, ' = ', $val, PHP_EOL;
+        }
+        echo PHP_EOL;
         exit(1);
+    }
+
+
+    /**
+     * Overwrite the context
+     */
+    public static function setContext(array $ctx)
+    {
+        self::$context = $ctx;
+    }
+
+
+    /**
+     * Add or update context fields
+     */
+    public static function addContext(array $ctx)
+    {
+        self::$context = array_merge(self::$context, $ctx);
+    }
+
+
+    /**
+     * Clear all context fields
+     */
+    public static function clearContext()
+    {
+        self::$context = [];
     }
 
 }
